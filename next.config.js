ac3d43c9f23b198c3e993e138803f004c875dbea
moniaaiofficial <moniaaiofficial@gmail.com>
2026-03-10 18:36:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const withPWA = require('next-pwa')({
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 module.exports = withPWA(nextConfig);
